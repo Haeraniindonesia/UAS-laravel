178ebd7e5598ff2a9416fcbe28851f1cc712f0d9
Haeraniindonesia <78776154+Haeraniindonesia@users.noreply.github.com>
2022-07-18 22:46:13 +0700

    commit1

---
+++ ProductController.php
@@ -0,0 +1,17 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Illuminate\Http\Request;
+
+class ProductController extends Controller
+
+{
+    //
+    public function showA11(){
+        $dataDariModel = "Data Product";
+        return view('product.display', ["products"=> $dataDariModel]);
+    }
+}
+
+
